effe0066143ecc000b2ef4d4d46f97c4f7385f6f
ekarp <emilyrkarp@gmail.com>
2018-10-18 10:07:05 -0400

    multiple paragraphs

--- lorem.py
+++ lorem.py
@@ -8,12 +8,14 @@ words = re.sub(r'[^ A-Za-z]', ' ', words)
 words = re.sub(r'\s+', ' ', words)
 words = words.split(' ')
 
-wordcount = 500 if len(sys.argv) == 1 else int(sys.argv[1])
+wordcounts = ['500'] if len(sys.argv) == 1 else sys.argv[1:]
 lorem = ''
 
-for i in range(wordcount):
-  word = words[random.randint(0, len(words) - 1)]
-  lorem += word.strip() + ' '
+for wordcount in wordcounts:
+  for i in range(int(wordcount)):
+    word = words[random.randint(0, len(words) - 1)]
+    lorem += word.strip() + ' '
 
-print(lorem)
+  print(lorem + '\n')
+
 
